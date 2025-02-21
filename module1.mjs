58c0e103b3022ac2c9334fbe8b81fc17904b1766
Vinay Kaushik <kaushikvinay.2001@gmail.com>
2025-02-21 22:13:16 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,12 @@
+const a = "Rohan";
+const b = "Rohit";
+const c = "Dhruv";
+const d = "Karan";
+const e = "Sunil";
+
+export default a;
+// export { b, c, d, e };
+export { b };
+export { c };
+export { d };
+export { e };
---
+++ module2.mjs
@@ -0,0 +1,6 @@
+import xyz, { b, c, d, e } from './module1.mjs'
+console.log(xyz);
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
